4f2cef7d74f7f6ac2a125f70c74be5a2ff4556c0
Dysprosium <dysprosium@outlook.com.br>
2023-01-04 14:18:42 -0300

    :dart: Feature: Personlizar Template Cadastro de Pessoa

--- src/app/page/profile/profile.module.ts
+++ src/app/page/profile/profile.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { IonicInputMaskModule } from '@thiagoprz/ionic-input-mask';
 import { ProfilePageRoutingModule } from './profile-routing.module';
 import { ProfilePage } from './profile.page';
 
@@ -11,7 +12,8 @@ import { ProfilePage } from './profile.page';
     FormsModule,
     IonicModule,
     ProfilePageRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    IonicInputMaskModule
   ],
   declarations: [ProfilePage]
 })
